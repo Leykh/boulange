ef0ba5b5101fbd7c5d98c1829229d72ac33a1109
Leykh <eykhell@gmail.com>
2022-05-07 19:47:43 +0200

    correction de afficherOrdis, admin et tab

--- vue/afficherOrdis.php
+++ vue/afficherOrdis.php
@@ -8,9 +8,11 @@
         <tr>
         <th scope="col">Id</th>
           <th scope="col">Image</th>
-          <th scope="col">Titre</th>
+          <th scope="col">Denomination</th>
           <th scope="col">Prix</th>
-          <th scope="col">Nombre de pages</th>
+          <th scope="col">Processeur</th>
+          <th scope="col">Ecran</th>
+          <th scope="col">Memoire vive</th>
           <th scope="col" colspan="3">Actions</th>
         </tr>
       </thead>
@@ -19,9 +21,11 @@
           <tr class="align-middle">
             <td scope="row"><?php echo $Ordi['id'] ?></td>
             <td><img width="80" src="public/images/<?php echo $Ordi['image']; ?>"></td>
-            <td><?php echo $Ordi['titre']; ?></td>
+            <td><?php echo $Ordi['denomination']; ?></td>
             <td><?php echo $Ordi['prix']; ?></td>
-            <td><?php echo $Ordi['nbPages']; ?></td>
+            <td><?php echo $Ordi['processeur']; ?></td>
+            <td><?php echo $Ordi['ecran']; ?></td>
+            <td><?php echo $Ordi['vive']; ?></td>
             <td><a href="index.php?action=lire&id=<?= $Ordi['id']; ?>" class="btn btn-info">Lire</a></td>
             <td><a href="index.php?action=modifier&id=<?= $Ordi['id']; ?>" class="btn btn-warning">Modifier</a></td>
             <td><a href="index.php?action=suppr&id=<?= $Ordi['id']; ?>" class="btn btn-danger">Supprimer</a></td>
